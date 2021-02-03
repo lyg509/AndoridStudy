48f54364891fd345f4b0288a4c8fc2f5c58ac354
lyg509 <dydrjs252@naver.com>
2021-02-03 19:09:56 +0900

    commit

--- app/src/main/java/com/example/logtest/MainActivity.java
+++ app/src/main/java/com/example/logtest/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.logtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "LogTest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void mOnClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnAssert:
+                Log.wtf(TAG, "Assert Message");
+                break;
+
+            case R.id.btnError:
+                Log.wtf(TAG, "Error Message");
+                break;
+            case R.id.btnWarn:
+                Log.wtf(TAG, "Warn Message");
+                break;
+            case R.id.btnInfo:
+                Log.wtf(TAG, "Info Message");
+                break;
+            case R.id.btnDebug:
+                Log.wtf(TAG, "Debug Message");
+                break;
+            case R.id.btnVerbose:
+                Log.wtf(TAG, "Verbose Message");
+                break;
+
+        }
     }
 }
